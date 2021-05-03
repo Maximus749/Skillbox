5ed6951252775dabe9b70dd53f9b0f60cd5cbc4e
Maximus749 <maximus749@gmail.com>
2021-05-04 01:15:33 +1100

    Выполнил задание

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,6 +3,15 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+import room_1
+import room_2
 
+# room_1.folks[0]
+print("В комнате room_1 живут: ", end="")
+for folk in room_1.folks:
+    print(folk, end=", ")
+print()
+print("В комнате room_2 живут: ", end="")
+for folk in room_2.folks:
+    print(folk)
 
